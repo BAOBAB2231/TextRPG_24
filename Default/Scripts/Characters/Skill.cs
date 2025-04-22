4f53c17f547a65ee46a98be821a67d12dd4a0e15
jochn96 <whtjdcks55@naver.com>
2025-04-22 21:22:17 +0900

    feat : skill 추가

---
+++ Default/Scripts/Characters/Skill.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8.Scripts.Characters
+{
+    public enum SkillType
+    {
+        SingleTarget,
+        RandomTarget,
+        AllTarget,
+        SelfTarget
+    }
+        public class Skills
+    {
+        public string Name { get; set; }
+
+        public int MPCost { get; set; }
+
+        public string Description { get; set; }
+
+        public float DamageMultiplier { get; set; }
+
+        public int NumberofTargets { get; set; }
+
+        public SkillType Type { get; set; }
+
+        public Skills(string name, int mpCost, string description, float damageMultiplier, SkillType type, int numberofTargets = 1)
+        {
+            Name = name;
+            MPCost = mpCost;
+            Description = description;
+
+            DamageMultiplier = damageMultiplier;
+
+            NumberofTargets = numberofTargets;
+
+            Type = type;
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
